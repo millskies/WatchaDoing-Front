5afd881124cd52debb226ebe2ee9a7df8204a428
Lisa Schwetlick <schwetlick.lisa@gmail.com>
2023-06-05 15:27:00 +0200

    started create event form

--- wdClient/src/components/CreateEvent.jsx
+++ wdClient/src/components/CreateEvent.jsx
@@ -1,5 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+const baseUrl = "http://localhost:5005/";
+let token = localStorage.getItem('authToken');
+
 export default function CreateEvent() {
+  const userId = token.
+  const [user, setUser] = useState(null);
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [dateTime, setDateTime] = useState();
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+    axios.get(baseUrl + `/users/${userId}`)
+  })
+
   return (
-    <div>CreateEvent</div>
-  )
+    <>
+      <h1>Create new event</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">Title</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">Description</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="date" className="form-label">When?</label>
+          <input type="date" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="location" className="form-label">Where?</label>
+          <input type="" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="icon" className="form-label">Select icon</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div class="dropdown">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >Dropdown button</button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li>
+              <a class="dropdown-item" href="#">Action</a>
+            </li>
+          </ul>
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Allow your friends to share this event
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">Create</button>
+      </form>
+    </>
+  );
 }
--- wdClient/src/contexts/auth.context.jsx
+++ wdClient/src/contexts/auth.context.jsx
@@ -3,57 +3,61 @@ import { createContext, useEffect, useState } from "react";
 
 const authContext = createContext();
 
-const baseUrl = 'http://localhost:5005/';
+const baseUrl = "http://localhost:5005/";
 
-function AuthProviderWrapper({children}){
+function AuthProviderWrapper({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    isAuthenticated();
+  }, []);
 
-    useEffect(()=>{
-        isAuthenticated();
-    }, []);
+  const getHeaders = () => {
+    let token = localStorage.getItem("authToken");
+    return { headers: { authorization: `Bearer ${token}` } };
+  };
 
-    const getHeaders = () => {
-        let token = localStorage.getItem('authToken');
-        return {headers: {authorization: `Bearer ${token}`}};
+  const isAuthenticated = () => {
+    //get a token:
+    let token = localStorage.getItem("authToken");
+    if (token) {
+      axios
+        .get(baseUrl + "/verify", {
+          headers: { authorization: `Bearer ${token}` },
+        })
+        .then(({ data }) => {
+          setIsLoggedIn(true);
+          setUser(data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setIsLoggedIn(false);
+          setUser(null);
+          setLoading(false);
+          console.log(err);
+        });
+    } else {
+      setIsLoggedIn(false);
+      setUser(null);
+      setLoading(false);
     }
+  };
 
-    const isAuthenticated = () => {
-        //get a token:
-        let token = localStorage.getItem('authToken');
-        if(token) {
-            axios.get(baseUrl + '/verify', {headers: {authorization: `Bearer ${token}`}})
-            .then(({data}) => {
-                setIsLoggedIn(true);
-                setUser(data);
-                setLoading(false);
-            })
-            .catch(err => {
-                setIsLoggedIn(false);
-                setUser(null);
-                setLoading(false);
-                console.log(err)
-            })
-        } else {
-            setIsLoggedIn(false);
-            setUser(null);
-            setLoading(false);
-        }
-    }
-
-    let exposedValues = {
-        isLoggedIn,
-        user,
-        loading,
-        baseUrl,
-        isAuthenticated,
-        getHeaders
-    }
-    return(<authContext.Provider value={exposedValues}>
-        {children}
-    </authContext.Provider>);
+  let exposedValues = {
+    isLoggedIn,
+    user,
+    loading,
+    baseUrl,
+    isAuthenticated,
+    getHeaders,
+  };
+  return (
+    <authContext.Provider value={exposedValues}>
+      {children}
+    </authContext.Provider>
+  );
 }
 
-export {authContext, AuthProviderWrapper};
+export { authContext, AuthProviderWrapper };
