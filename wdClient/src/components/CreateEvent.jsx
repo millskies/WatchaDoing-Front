a2ba79907bea4991e07ce48112d971be849efdd4
Lisa Schwetlick <schwetlick.lisa@gmail.com>
2023-06-05 18:03:55 +0200

    working on fix for profile page

--- wdClient/src/components/CreateEvent.jsx
+++ wdClient/src/components/CreateEvent.jsx
@@ -1,40 +1,37 @@
 import axios from "axios";
-import { useEffect, useState, useContext } from "react";
-import { authContext } from "../contexts/auth.context"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const baseUrl = "http://localhost:5005/";
-let token = localStorage.getItem('authToken');
 
 export default function CreateEvent() {
-  // const userId = token.  ---> how do I get the userId from the context?
-  const [user, setUser] = useState(null);
+  
+  const {username} = useParams;
+
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [date, setDate] = useState(null);
   const [time, setTime] = useState(null);
   const [location, setLocation] = useState(null);
-  // const [invited, setInvited] = useState([]);
+  const [dropdownData, setDropdownData] = useState([]);
+  const [selectedItems, setSelectedItems] = useState([]);
 
-  const dropDown = () => {
-    const [dropdownData, setDropdownData] = useState([]);
-    const [selectedItems, setSelectedItems] = useState([]);
+  // const handleItemSelected = (item) => {
+  //   setSelectedItems([...selectedItems, item._id]);
+  // };
 
-    const handleItemSelected = (item) => {
-      setSelectedItems([...selectedItems, item]);
-    };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    let newEvent = { title, description, date, time, location, invited };
+  // const submitHandler = (e) => {
+  //   e.preventDefault();
+  //   let newEvent = { title, description, date, time, location, selectedItems };
     // createEventFunc(newEvent);
-    createEvent(newEvent)}
+    // createEvent(newEvent)
+  // };
 
   useEffect(() => {
-    axios.get(baseUrl + `/users/${userId}`)
+    axios.get(baseUrl + `/users/${username}`)
     .populate('inviteLists')
     .populate('friendsConfirmed')
     .then((user) => {
-      setUser(user);
       let friendsAndLists = [user.friendsConfirmed, user.inviteLists]
       setDropdownData(friendsAndLists)
     })
@@ -59,29 +56,29 @@ export default function CreateEvent() {
         </div>
         <div className="mb-3">
           <label htmlFor="date" className="form-label">What time?</label>
-          <input type="date" className="form-control" value={time} onChange={(e) => setTime(e.target.value)}/>
+          <input type="time" className="form-control" value={time} onChange={(e) => setTime(e.target.value)}/>
         </div>
         <div className="mb-3">
           <label htmlFor="location" className="form-label">Where?</label>
           <input type="" className="form-control" value={location} onChange={(e) => setLocation(e.target.value)}/>
         </div>
-        <div className="mb-3">
+        {/* <div className="mb-3">
           <label htmlFor="icon" className="form-label">Select icon</label>
-          <input type="text" className="form-control" value={icon} onChange={(e) => setTitle(e.target.value)}/>
-        </div>
+          <input type="text" className="form-control" onChange={(e) => setTitle(e.target.value)}/>
+        </div> */}
 
         {/* Dropdown to select friends & lists to invite */}
-        <div class="dropdown">
+        <div className="dropdown">
           <button
-            class="btn btn-secondary dropdown-toggle"
+            className="btn btn-secondary dropdown-toggle"
             type="button"
             id="dropdownMenuButton1"
             data-bs-toggle="dropdown"
             aria-expanded="false"
           >Invite friends</button>
-        <select class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <select className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
           {dropdownData.map((item) => (
-        <option class="dropdown-item" key={item.id} onClick={() => handleItemSelected(item)}>
+        <option className="dropdown-item" key={item.id} onClick={() => handleItemSelected(item)}>
           {item.username}
         </option>
       ))}
