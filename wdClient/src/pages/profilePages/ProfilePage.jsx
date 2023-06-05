11c8002952d2a842855b188c423c9ae398d61f48
Raquel Barrio <r.barrio.f@gmail.com>
2023-06-05 15:00:58 +0200

    trying to get reloading profile to work

--- wdClient/src/pages/profilePages/ProfilePage.jsx
+++ wdClient/src/pages/profilePages/ProfilePage.jsx
@@ -6,17 +6,38 @@ import OwnProfilePage from "./OwnProfilePage";
 import PublicProfilePage from "./PublicProfilePage";
 
 export default function ProfilePage() {
-  const { username } = useParams();
-  // console.log("username: ", username)
   const { user, baseUrl } = useContext(authContext);
+  console.log("useeer:",user)
+  const { username } = useParams();
+  console.log("username: ", username)
   const navigate = useNavigate();
 
-  const [ownProfile, setOwnProfile] = useState(true); // changeLater
+  const [ownProfile, setOwnProfile] = useState(false); // changeLater
   const [publicProfile, setPublicProfile] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  if (user && username == user.username) {
-    setOwnProfile(true);
-  }
+
+  useEffect(() => {
+      if (username == user.username) {
+        console.log("useeertimeout:",user)
+        setOwnProfile(true);
+        setPublicProfile(false);
+      } else if (username != user.username) {
+        axios
+          .get(baseUrl + "/users/" + username)
+          .then((response) => {
+            console.log("response: ", response);
+            setPublicProfile(true);
+            setOwnProfile(false);
+          })
+          .catch((err) => {
+            console.log(err);
+            navigate("/404");
+          });
+      }
+    // console.log("params: ", params)
+    
+  }, [username]);
 
   // Check if the username profile route we are trying to access belongs to a real user or should redirect to an error page.
 
@@ -25,8 +46,9 @@ export default function ProfilePage() {
   // useEffect(() => {
   //   if (user && username != user.username) {
   //     axios
-  //       .get(baseUrl + "users/" + username)
-  //       .then(setPublicProfile(true))
+  //       .get(baseUrl + "/users/" + username)
+  //       .then(setPublicProfile(true);
+  //       setOwnProfile(false))
   //       .catch((err) => {
   //         console.log(err);
   //         navigate("/404");
@@ -38,17 +60,18 @@ export default function ProfilePage() {
 
   return (
     <div>
-      {ownProfile && (
+    blabla
+      {/* {ownProfile && (
         <div>
           <OwnProfilePage />
         </div>
       )}
-      
+
       {publicProfile && (
         <div>
           <PublicProfilePage />
         </div>
-      )}
+      )} */}
     </div>
   );
 }
