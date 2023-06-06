c6796c9c235057416eef9b25b67743085b2ee3a7
Lisa Schwetlick <schwetlick.lisa@gmail.com>
2023-06-06 11:38:14 +0200

    commiting before pulling reload fix

--- wdClient/src/components/CreateEvent.jsx
+++ wdClient/src/components/CreateEvent.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
-const baseUrl = "http://localhost:5005/";
+import { useEffect, useState, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { authContext } from "../contexts/auth.context";
 
 export default function CreateEvent() {
   
   const {username} = useParams;
-//State variables 
+  const navigate = useNavigate();
+  const baseUrl = useContext(authContext);
+  
+  //State variables 
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [date, setDate] = useState(null);
@@ -20,32 +22,44 @@ export default function CreateEvent() {
     setSelectedItems([...selectedItems, item._id]);
   };
 
+  //calls the backend route to create the event
   const submitHandler = (e) => {
     e.preventDefault();
     let newEvent = { title, description, date, time, location, selectedItems };
-    createEventFunc(newEvent);
-    createEvent(newEvent)
+    axios.post(baseUrl + "/events/create", newEvent)
+    .then((res) => {
+      console.log("event created", res)
+    })
+    .catch((err) => {
+      console.log(err);
+      navigate("/404");
+    });
   };
 
+  //Fetching user friends + invitelists to populate the dropdown and allow to select friends to invite
   useEffect(() => {
     axios.get(baseUrl + `/users/${username}`)
     .then((user) => {
-      let friendsAndLists = [user.friendsConfirmed, user.inviteLists]
+      let friendsAndLists = [...user.friendsConfirmed, ...user.inviteLists]
       setDropdownData(friendsAndLists)
     })
-    .catch(err => console.log(err))
+    .catch((err) => {
+      console.log(err);
+      navigate("/404");
+    });
   }, [])
+
 
   return (
     <>
       <h1>Create new event</h1>
       <form onSubmit={submitHandler}>
         <div className="mb-3">
-          <label htmlFor="title" className="form-label">Title</label>
+          <label htmlFor="title" className="form-label">What will you be doing?</label>
           <input type="text" className="form-control" value={title} onChange={(e) => setTitle(e.target.value)}/>
         </div>
         <div className="mb-3">
-          <label htmlFor="description" className="form-label">Description</label>
+          <label htmlFor="description" className="form-label">Add a few more details, eg. what to bring, if tickets need to be bought, ...</label>
           <input type="text" className="form-control" value={description} onChange={(e) => setDescription(e.target.value)}/>
         </div>
         <div className="mb-3">
