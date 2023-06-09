fd375fb0da3a0d42b1de9b44bf5a8547c5621d4a
Raquel Barrio <r.barrio.f@gmail.com>
2023-06-09 10:15:28 +0200

    changed baseUrl

--- wdClient/src/contexts/auth.context.jsx
+++ wdClient/src/contexts/auth.context.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 
 const authContext = createContext();
 
-const baseUrl = "http://localhost:5005";
+const baseUrl = import.meta.env.VITE_API_URL;
 
 function AuthProviderWrapper({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false); //false
